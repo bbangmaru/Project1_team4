1cbbf27a8440fa4ec3b1551c48c806f808e0b14d
bbangmaru <hobin1217@soongsil.ac.kr>
2022-04-04 00:12:35 +0900

    change2
    Clustering 및 Tree-search DFS 적용
    (아직 시작도시 설정은 안함)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import numpy as np
+import pandas as pd
+
 from CalculationDist import Calculation # 계산 담당
 from ClusteringCity import Clustering # 클러스터링 담당
 from MakeCrossover import Crossover # 교차연산 담당
@@ -7,32 +9,23 @@ from MakeMutation import Mutation # 변이 담당
 from Selection import Select # 선택 담당 - fitness 함수를 정의하면 됨
 from Tree import TreeSearch # 트리 search 담당
 
+df = pd.read_csv("TSP.csv", header=None, names=['x', 'y'])
+#print(df)
 cities = []
-sol = []
-order = []
-
-#1. get solution sequence and reordering (sort from 0)
-with open('example_solution.csv', mode='r', newline='') as solution:
-    # read solution sequence
-    reader = csv.reader(solution)
-    for row in reader:
-        sol.append(int(row[0]))
-    # reordering solution sequence, 해답 sequence를 재배치
-    idx = sol.index(0) # 189
-
-    front = sol[idx:] # 189 ~ 1000
-
-    back = sol[0:idx] # 0 ~ 189
-
-    sol = front + back # 두개를 합침
-    # expand 0 city(start) for simplicity, 간단하게 생각하기 위해 도시 0부터 시작하는 걸로!
-    sol.append(int(0))
-# 2. get TSP city map
 with open('TSP.csv', mode='r', newline='') as tsp:
-    # read TSP city map
     reader = csv.reader(tsp)
-    for row in reader:
+    for row in reader :
         cities.append(row)
+# kcluster instance 생성
+kcluster = Clustering()
+k = 10
+center_city_coord, child_city_idx, child_city_coord = kcluster.clusterTSP(df, k, cities)
+picked = [0 for _ in range(k)]
+tree_search_cost, tree_search_path = TreeSearch.DFS(picked, center_city_coord, k, 0)
+print(tree_search_cost)
+print(tree_search_path)
 
 
-# 여기까진 첨부 자료
+
+
+
