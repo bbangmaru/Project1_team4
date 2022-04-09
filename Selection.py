ffb283fc06ed4b6fc3b44f0f1fb5e1715f121326
박근형 <w8385@naver.com>
2022-04-09 20:46:12 +0900

    return index[2] in Selection.py

--- Selection.py
+++ Selection.py
@@ -13,15 +13,24 @@ class Select:
     @staticmethod
     def roulette_wheel(fitness):
         fitnessSum = 0
-        idx = 0
+        idx = [-1, -1]
         for i in range(len(fitness)):
             fitnessSum += fitness[i]
-        print("fitnessSum: ", fitnessSum)
-        point = random.uniform(0, fitnessSum)
-        print("point: ", point)
-        while point > 0:
-            point -= fitness[idx]
-            idx += 1
-        idx -= 1
-        return idx
+        print("fitnessSum   : ", fitnessSum)
+
+        point1 = random.uniform(0, fitnessSum)
+        while point1 > 0:
+            idx[0] += 1
+            point1 -= fitness[idx[0]]
+
+        while 1:
+            point2 = random.uniform(0, fitnessSum)
+            while point2 > 0:
+                idx[1] += 1
+                point2 -= fitness[idx[1]]
+            if idx[0] != idx[1]:
+                print("idx          : ", idx)
+                return idx
+
+
 
